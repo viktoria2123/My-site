5f35dc21bd770c2ccff5aa26c2c8072493f5e4c4
viktoria2123 <v.andreeva2123@gmail.com>
2022-11-19 11:30:10 +0100

    animation

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,19 @@
 console.log("test");
+let h1 = document.querySelectorAll('.animation');
+let active = 'animation__active';
+
+let isInViewport = function (elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+addEventListener('scroll', (event) => {
+    for (let i = 0; i < h1.length; i++) {
+        if (isInViewport(h1[i])) {
+            h1[i].classList.add(active);
+        }
+    }
+});
